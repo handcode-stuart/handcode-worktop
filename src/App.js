92611ac10c8c7752840d2d4044643f007dfe096f
Stuart Morris <admin@handcode.co.uk>
2019-07-23 10:52:41 +0100

    Render out basic list of projects in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from "react";
 
+const data = [
+    {
+        id: 1,
+        name: "CRM",
+        github: "https://github.com/handcode-stuart/crm",
+        website: "https://www.handcode.co.uk",
+    },
+    {
+        id: 2,
+        name: "Orderly",
+        github: "https://github.com/handcode-stuart/orderly",
+        website: "https://www.handcode.co.uk",
+    },
+];
+
 const App = () => {
     return (
         <div>
             <h1>App</h1>
+            <ul>
+                {data.map(proj => (
+                    <li key={proj.id}>
+                        {proj.name}
+                        <a href={proj.github} target='_blank' rel='noopener noreferrer'>
+                            Code
+                        </a>
+                        <a href={proj.website} target='_blank' rel='noopener noreferrer'>
+                            Website
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,8 +2,63 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../../App";
 
+let wrapper;
+
+const data = [
+    {
+        id: 1,
+        name: "CRM",
+        github: "https://github.com/handcode-stuart/crm",
+        website: "https://www.handcode.co.uk",
+    },
+    {
+        id: 2,
+        name: "Orderly",
+        github: "https://github.com/handcode-stuart/orderly",
+        website: "https://www.handcode.co.uk",
+    },
+];
+
 describe("<App />", () => {
+    beforeEach(() => {
+        wrapper = shallow(<App />);
+    });
+
     it("should render", () => {
-        shallow(<App />);
+        expect(wrapper).toBeTruthy();
+    });
+
+    it("should render a list of projects", () => {
+        expect(wrapper.find("li")).toHaveLength(2);
+
+        expect(wrapper.find("Orderly")).toBeTruthy();
+    });
+
+    it("should render a link for the github of a project", () => {
+        expect(
+            wrapper.contains(
+                <a
+                    href='https://github.com/handcode-stuart/crm'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Code
+                </a>,
+            ),
+        ).toBeTruthy();
+    });
+
+    it("should render a link for the website of a project", () => {
+        expect(
+            wrapper.contains(
+                <a href='https://www.handcode.co.uk' target='_blank' rel='noopener noreferrer'>
+                    Website
+                </a>,
+            ),
+        ).toBeTruthy();
+    });
+
+    it("should render the name of a project", () => {
+        expect(wrapper.find("CRM")).toBeTruthy();
     });
 });
